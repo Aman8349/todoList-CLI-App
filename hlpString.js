3a305102941fe3ddb9bb3a34992eb6d2ad035fd8
Aman Sahu <sahua8349@gmail.com>
2021-01-04 13:38:56 +0530

    final commit

---
+++ hlpString.js
@@ -0,0 +1,3 @@
+const helpString =
+  'Usage :-\n$ ./todo add "todo item"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics';
+module.exports = helpString;
